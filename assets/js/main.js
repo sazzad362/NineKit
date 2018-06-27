983bf661727ff93455cc435aa5bc9b247a0ce301
Md Sazzad Hossain <sazzad362@gmail.com>
2018-06-27 15:52:50 +0600

    Left sidebar toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $("#cat_acro").click(function(){
         $("#togle").slideToggle(1000);
     });
+     $("#menu_mb").click(function(){
+        $("#menu_show").slideToggle(1000);
+    });
 });
